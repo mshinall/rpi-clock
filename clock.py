e9a583a387755d70a490dc9c4763f1bce57ba716
Matt Shinall <matt.shinall@gmail.com>
2018-10-21 11:34:03 -0400

    update

--- clock.py
+++ clock.py
@@ -4,7 +4,7 @@ import time
 import I2C_LCD_driver
 
 mylcd = I2C_LCD_driver.lcd()
-ticker = ["|", "/", "-", "\\"]
+ticker = ["|", "/", "-"] #, "\\"]
 t = 0
 
 def clearLcd():
@@ -18,10 +18,13 @@ def updateLcd():
 		t = 0
 
 	mylcd.lcd_display_string("Hello", 1, 1)
+	"""
 	#mylcd.lcd_display_string(time.strftime("%a, %d %b %Y", time.localtime()), 1, 0)
 	#mylcd.lcd_display_string(time.strftime("%H:%M:%S", time.localtime()), 2, 0)
 	#mylcd.lcd_display_string(ticker[i], 4, 19)
+	"""
 
+"""
 try:
 	updateLcd()
 	while True:
@@ -29,3 +32,4 @@ try:
 
 except:
 	clearLcd()
+"""
