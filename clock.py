c61ab351ed274a8095979f005c16af7e46a3fb3b
Matt Shinall <matt.shinall@gmail.com>
2018-10-21 12:50:41 -0400

    update

--- clock.py
+++ clock.py
@@ -4,31 +4,30 @@ import time
 from weather import Weather, Unit
 import I2C_LCD_driver
 
+
 mylcd = I2C_LCD_driver.lcd()
 weather = Weather(unit=Unit.CELSIUS)
-w = 0;
-outlook = "";
+w = 0
+outlook = ""
 
 def clearLcd():
 	global mylcd
 	mylcd.lcd_clear()
 
 def updateLcd():
-	global mylcd, ticker, w
+	global mylcd, ticker, w, q
 	w += 1
 	if w >= 599:
 		updateWeather()
 		w = 0
-	"""
-	mylcd.lcd_display_string("Hello", 1, 1)
-	"""
+
 	mylcd.lcd_display_string(time.strftime("%a, %d %b %Y", time.localtime()), 1, 0)
 	mylcd.lcd_display_string(time.strftime("%H:%M:%S", time.localtime()), 2, 0)
 	mylcd.lcd_display_string(outlook, 3, 0)
 
 def updateWeather():
 	global outlook
-	lookup = weather.lookup(560743)
+	lookup = weather.lookup(28350089)
 	condition = lookup.condition
 	outlook = condition.text
 
