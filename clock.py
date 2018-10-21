f11fc5ff848e14c1350a13da72da928e126a884d
Matt Shinall <matt.shinall@gmail.com>
2018-10-21 13:00:58 -0400

    update

--- clock.py
+++ clock.py
@@ -7,29 +7,36 @@ import I2C_LCD_driver
 
 mylcd = I2C_LCD_driver.lcd()
 weather = Weather(unit=Unit.CELSIUS)
+wlocs = [28350089, 2499644]
+wnames = ["Martinsburg", "Sterling"]
 w = 0
-outlook = ""
+wl = 0
+outlooks = ["",""]
 
 def clearLcd():
 	global mylcd
 	mylcd.lcd_clear()
 
 def updateLcd():
-	global mylcd, ticker, w, q
+	global mylcd, w, wl, wlocs
 	w += 1
+	wl += 1
 	if w >= 599:
 		updateWeather()
 		w = 0
+		if(wl >= len(wlocs)):
+			wl = 0
 
 	mylcd.lcd_display_string(time.strftime("%a, %d %b %Y", time.localtime()), 1, 0)
 	mylcd.lcd_display_string(time.strftime("%H:%M:%S", time.localtime()), 2, 0)
-	mylcd.lcd_display_string(outlook, 3, 0)
+	mylcd.lcd_display_string(wnames[wl] + " " + outlooks[wl], 3, 0)
 
 def updateWeather():
-	global outlook
-	lookup = weather.lookup(28350089)
-	condition = lookup.condition
-	outlook = condition.text
+	global outlooks
+	for i in range(0, len (wlocs)):
+		lookup = weather.lookup(wlocs[wl])
+		condition = lookup.condition
+		outlook = condition.text
 
 try:
 	updateWeather()
