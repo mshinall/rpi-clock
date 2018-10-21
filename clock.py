011744c9cc6b468a6741ef6634ac9ad1604646a1
Matt Shinall <matt.shinall@gmail.com>
2018-10-21 11:39:20 -0400

    update

--- clock.py
+++ clock.py
@@ -16,13 +16,12 @@ def updateLcd():
 	t += 1
 	if t >= len(ticker):
 		t = 0
-
+	"""
 	mylcd.lcd_display_string("Hello", 1, 1)
 	"""
-	#mylcd.lcd_display_string(time.strftime("%a, %d %b %Y", time.localtime()), 1, 0)
-	#mylcd.lcd_display_string(time.strftime("%H:%M:%S", time.localtime()), 2, 0)
-	#mylcd.lcd_display_string(ticker[i], 4, 19)
-	"""
+	mylcd.lcd_display_string(time.strftime("%a, %d %b %Y", time.localtime()), 1, 0)
+	mylcd.lcd_display_string(time.strftime("%H:%M:%S", time.localtime()), 2, 0)
+	mylcd.lcd_display_string(ticker[t], 4, 19)
 
 try:
 	updateLcd()
