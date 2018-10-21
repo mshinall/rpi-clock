af154e9721d8b34e2dd107c0a785b17e3bcea0e9
Matt Shinall <matt.shinall@gmail.com>
2018-10-21 11:29:26 -0400

    update

--- clock.py
+++ clock.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
-import I2C_LCD_driver
 import time
+import I2C_LCD_driver
 
 mylcd = I2C_LCD_driver.lcd()
 ticker = ["|", "/", "-", "\\"]
@@ -15,15 +15,16 @@ def updateLcd():
 	global mylcd, ticker
 	t += 1
 	if t >= length(ticker):
-		t = 0;
+		t = 0
 
-	mylcd.lcd_display_string(time.strftime("%a, %d %b %Y", time.localtime()), 1, 0)
-	mylcd.lcd_display_string(time.strftime("%H:%M:%S", time.localtime()), 2, 0)
-	mylcd.lcd_display_string(ticker[i], 4, 19)
+	mylcd.lcd_display_string("Hello", 1, 1)
+	#mylcd.lcd_display_string(time.strftime("%a, %d %b %Y", time.localtime()), 1, 0)
+	#mylcd.lcd_display_string(time.strftime("%H:%M:%S", time.localtime()), 2, 0)
+	#mylcd.lcd_display_string(ticker[i], 4, 19)
 
 try:
 	updateLcd()
-	while(True):
+	while True:
 		time.sleep(0.2)
 
 except:
