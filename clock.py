52accdc037efbead1b55c60397a0fc23933f52c2
Matt <matt.shinall@gmail.com>
2018-10-27 11:29:37 -0400

    updated

--- clock.py
+++ clock.py
@@ -11,14 +11,17 @@ import I2C_LCD_driver
 mylcd = I2C_LCD_driver.lcd()
 weather = Weather(unit=Unit.FAHRENHEIT)
 weatherLocations = [28350089, 2499644, 2409835, 2428344]
-weatherCityNames = ["Martinsburg WV", "Sterling VA", "Germantown MD", "Jacksonville FL"]
-weatherOutlooks = ["", "", "", ""]
-weatherOutlookIdx = 0;
+weatherCityNames = ["" for w in range(0,len(weatherLocations))]
+weatherOutlookCt = 4
+weatherOutlooks = [["" for w in range(0,len(weatherLocations))] for z in range(0,weatherOutlookCt)]
+weatherOutlookIdx = 0
+weatherLocationIdx = 0
 #degrees = u'\N{DEGREE SIGN}'
 degrees = '*'
 args = sys.argv[1:]
 argIdx = 0
-
+baro = ["steady", "rising", "falling"]
+lcdRefreshInt = 1.0
 oldLcdBuffer = [[" " for x in range(0, 20)] for y in range(0,4)]
 newLcdBuffer = [[" " for x in range(0, 20)] for y in range(0,4)]
 
@@ -44,18 +47,6 @@ def clearLcd():
 	global mylcd
 	mylcd.lcd_clear()
 
-"""
-def updateLcd():
-	printBuffers()
-	global mylcd, newLcdBuffer, oldLcdBuffer
-	for i in range(0, 4):
-		if oldLcdBuffer[i] != newLcdBuffer[i]:
-			if i > 1:
-				mylcd.lcd_display_string(' ' * len(oldLcdBuffer[i]), i+1, 0)
-			mylcd.lcd_display_string(newLcdBuffer[i], i+1, 0)
-			oldLcdBuffer[i] = newLcdBuffer[i]
-"""
-
 def updateLcd():
 	global mylcd, newLcdBuffer, oldLcdBuffer
 	#printBuffers()
@@ -70,9 +61,9 @@ def updateTimeBuffer():
 	lcdBuffer(1, time.strftime("%m/%d/%Y  %I:%M %p", now))
 
 def updateWeatherBuffer():
-	global weatherCityNames, weatherOutlooks, weatherOutlookIdx
-	lcdBuffer(2, weatherCityNames[weatherOutlookIdx])
-	lcdBuffer(3, weatherOutlooks[weatherOutlookIdx])
+	global weatherCityNames, weatherOutlooks, weatherOutlookIdx, weatherLocationIdx
+	lcdBuffer(2, weatherCityNames[weatherLocationIdx])
+	lcdBuffer(3, weatherOutlooks[weatherLocationIdx][weatherOutlookIdx])
 
 def lcdBuffer(y, string):
 	global newLcdBuffer
@@ -80,7 +71,8 @@ def lcdBuffer(y, string):
 
 def updateArgBuffer():
 	global argIdx, args
-	lcdBuffer(0, args[argIdx])	
+	if len(args) > 0:
+		lcdBuffer(0, args[argIdx])	
 
 def rotateArg():
 	global argIdx, args
@@ -90,24 +82,39 @@ def rotateArg():
 	updateArgBuffer()
 
 def rotateWeather():
-	global weatherOutlookIdx, weatherLocations, weatherRotateTimer
+	global weatherOutlookIdx, weatherLocations, weatherRotateTimer, weatherLocationIdx, weatherOutlookCt
 	weatherOutlookIdx += 1
-	if weatherOutlookIdx >= len(weatherLocations):
+	if weatherOutlookIdx >= weatherOutlookCt:
 		weatherOutlookIdx = 0
+		weatherLocationIdx += 1
+		if weatherLocationIdx >= len(weatherLocations):
+			weatherLocationIdx = 0
 	updateWeatherBuffer()
 
+def degrees_to_cardinal(d):
+	'''
+	note: this is highly approximate...
+	'''
+	dirs = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"]
+	ix = int((d + 11.25)/22.5)
+	return dirs[ix % 16]
+
 def updateWeather():
-	global weatherOutlooks, weatherLocations, weatherUpdateTimer, weatherCityNames
+	global weatherOutlooks, weatherLocations, weatherUpdateTimer, weatherCityNames, baro
 	for i in range(0, len(weatherLocations)):
 		lookup = weather.lookup(weatherLocations[i])
 		condition = lookup.condition
-		weatherOutlooks[i] = condition.temp + "F " + condition.text
-		print str(weatherLocations[i]) + ": " + weatherCityNames[i] + ": " + weatherOutlooks[i]
+		weatherOutlooks[i][0] = condition.temp + "F " + condition.text
+		weatherCityNames[i] = lookup.location.city + lookup.location.region
+		#print str(weatherLocations[i]) + ": " + weatherCityNames[i] + ": " + weatherOutlooks[i]
+		weatherOutlooks[i][1] = "Wind " + lookup.wind.speed + lookup.units.speed + " " + lookup.wind.chill + lookup.units.temperature + " " + degrees_to_cardinal(int(lookup.wind.direction))
+		weatherOutlooks[i][2] = "Humidity " + lookup.atmosphere.humidity + "%"
+		weatherOutlooks[i][3] = "Pres " + lookup.atmosphere.pressure + lookup.units.pressure + " " + baro[int(lookup.atmosphere.rising)]
 	updateWeatherBuffer()
 
-weatherRotateTimer = Timer(5.0, rotateWeather)
+weatherRotateTimer = Timer(2.0, rotateWeather)
 weatherUpdateTimer = Timer(1800.0, updateWeather)
-argUpdateTimer = Timer(2.0, rotateArg)
+argUpdateTimer = Timer(4.0, rotateArg)
 
 try:
 	weatherUpdateTimer.start()
@@ -118,7 +125,7 @@ try:
 	while True:
 		updateTimeBuffer()
 		updateLcd()
-		time.sleep(1)
+		time.sleep(lcdRefreshInt)
 except:
 	clearLcd()
 finally:
